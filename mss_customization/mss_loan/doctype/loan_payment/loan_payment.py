6afc1fb89c9fef317fcc4ea9a195080319fd1213
Sun Howwrongbum <sun@adrenosapient.com>
2018-03-13 00:27:14 +0530

    Loan Payment: add behaviour
    - generate interest child table
    - create gl entries
    - extend and reset foreclosure dates of loans

--- mss_customization/mss_loan/doctype/loan_payment/loan_payment.py
+++ mss_customization/mss_loan/doctype/loan_payment/loan_payment.py
@@ -4,7 +4,108 @@
 
 from __future__ import unicode_literals
 import frappe
-from frappe.model.document import Document
+from frappe.utils import add_months, cint
+from erpnext.controllers.accounts_controller import AccountsController
+from erpnext.accounts.general_ledger import make_gl_entries
+from erpnext.accounts.doctype.sales_invoice.sales_invoice \
+    import get_bank_cash_account
+from mss_customization.utils.queries import get_paid_interests, get_outstanding
+from mss_customization.utils.transform import make_period
 
-class LoanPayment(Document):
-	pass
+
+class LoanPayment(AccountsController):
+    def before_save(self):
+        gold_loan = frappe.get_doc('Gold Loan', self.loan)
+        if not self.mode_of_payment:
+            self.mode_of_payment = gold_loan.mode_of_payment
+        if not self.payment_account:
+            self.payment_account = get_bank_cash_account(
+                self.mode_of_payment, self.company
+            ).get('account')
+        if not self.loan_account:
+            self.loan_account = gold_loan.loan_account
+        if not self.interest_income_account:
+            self.interest_income_account = gold_loan.interest_income_account
+        outstanding = get_outstanding(
+            gold_loan.name, posting_date=self.posting_date
+        )
+        interest = outstanding * gold_loan.interest / 100.0
+        self.total_interest = self.interest_months * interest
+        if self.interest_months > 0:
+            self.make_interests(gold_loan, interest)
+
+    def make_interests(self, loan, interest_amount):
+        paid_interests = get_paid_interests(
+            self.loan, posting_date=self.posting_date
+        )
+        interest_days = map(
+            lambda x: add_months(loan.posting_date, x),
+            range(
+                paid_interests.count,
+                self.interest_months + paid_interests.count
+            )
+        )
+        for day in interest_days:
+            self.append('interests', {
+                'period_label': '',
+                'period_code': make_period(day),
+                'interest_amount': interest_amount
+            })
+
+    def on_submit(self):
+        self.make_gl_entries()
+        self.update_foreclosure_date()
+
+    def on_cancel(self):
+        self.make_gl_entries(cancel=1)
+        self.update_foreclosure_date()
+
+    def get_gl_dict(self, args):
+        gl_dict = frappe._dict({
+                'against_voucher_type': 'Gold Loan',
+                'against_voucher': self.loan
+            })
+        gl_dict.update(args)
+        return super(LoanPayment, self).get_gl_dict(gl_dict)
+
+    def make_gl_entries(self, cancel=0):
+        gl_entries = []
+        if self.interest_months > 0:
+            gl_entries.append(
+                self.get_gl_dict({
+                    'account': self.payment_account,
+                    'debit': self.total_interest,
+                    'against': self.customer,
+                })
+            )
+            gl_entries.append(
+                self.get_gl_dict({
+                    'account': self.interest_income_account,
+                    'credit': self.total_interest,
+                    'cost_center': frappe.db.get_value(
+                        'MSS Loan Settings',
+                        None,
+                        'cost_center'
+                    ),
+                    'against': self.customer,
+                })
+            )
+        make_gl_entries(gl_entries, cancel=cancel, adv_adj=0)
+
+    def update_foreclosure_date(self):
+        """
+            Updates foreclosure date of the Loan.
+            Also works for on cancel because paid_interests contains only items
+            with docstatus = 1 and this method is run after the cancel is
+            complete
+        """
+        paid_interests = get_paid_interests(self.loan)
+        months_to_foreclosure = frappe.get_value(
+            'MSS Loan Settings', None, 'months_to_foreclosure'
+        )
+        loan = frappe.get_doc('Gold Loan', self.loan)
+        loan.foreclosure_date = add_months(
+            loan.posting_date,
+            cint(months_to_foreclosure) + paid_interests.count
+        )
+        loan.save()
--- mss_customization/mss_loan/doctype/loan_payment/test_loan_payment.py
+++ mss_customization/mss_loan/doctype/loan_payment/test_loan_payment.py
@@ -4,8 +4,9 @@
 
 from __future__ import unicode_literals
 
-import frappe
 import unittest
+import frappe
+from frappe.utils import getdate
 from mss_customization.mss_loan.doctype.gold_loan.test_gold_loan \
     import make_gold_loan
 from mss_customization.utils.queries import get_gle_by
@@ -14,34 +15,61 @@ get_loan_payment_gle = get_gle_by('Loan Payment')
 
 
 class TestLoanPayment(unittest.TestCase):
-    loan_fixture = None
-    fixture = None
+    loan_name_fixture = None
+    fixtures = []
 
     def setUp(self):
-        self.loan_fixture = make_gold_loan(posting_date='2017-08-19')
+        self.loan_name_fixture = make_gold_loan(posting_date='2017-08-19').name
 
     def tearDown(self):
-        if self.fixture:
-            if self.fixture.docstatus == 1:
-                self.fixture.cancel()
+        if len(self.fixtures) > 0:
+            for fixture in self.fixtures:
+                if fixture.docstatus == 1:
+                    fixture.cancel()
+                frappe.delete_doc_if_exists(
+                    'Loan Payment', fixture.name, force=1
+                )
+        if self.loan_name_fixture:
+            loan = frappe.get_doc('Gold Loan', self.loan_name_fixture)
+            loan.cancel()
             frappe.delete_doc_if_exists(
-                'Loan Payment', self.fixture.name, force=1
-            )
-            self.fixture = None
-        if self.loan_fixture:
-            self.loan_fixture.cancel()
-            frappe.delete_doc_if_exists(
-                'Gold Loan', self.loan_fixture.name, force=1
+                'Gold Loan', self.loan_name_fixture, force=1
             )
             self.loan_fixture = None
+        frappe.db.commit()
+
+    def test_total_interest(self):
+        payment = make_loan_payment(
+            loan=self.loan_name_fixture,
+            interest_months=2,
+            do_not_submit=True,
+        )
+        self.fixtures = [payment]
+        self.assertEqual(payment.total_interest, 1000.0)
+
+    def test_adds_proper_interest(self):
+        first = make_loan_payment(loan=self.loan_name_fixture)
+        self.fixtures = [first]
+        payment = make_loan_payment(
+            loan=self.loan_name_fixture,
+            posting_date='2017-10-21',
+            interest_months=2,
+            do_not_submit=True,
+        )
+        self.fixtures.append(payment)
+        self.assertEqual(len(payment.interests), 2)
+        for interest in payment.interests:
+            self.assertEqual(interest.interest_amount, 500)
+            self.assertIn(interest.period_code, [
+                '2017-09-19 - 2017-10-18', '2017-10-19 - 2017-11-18'
+            ]),
 
     def test_gl_entries(self):
-        payment = make_loan_payment(loan=self.loan_fixture.name)
-        self.fixture = payment
+        payment = make_loan_payment(loan=self.loan_name_fixture)
+        self.fixtures = [payment]
         exp_gle = dict((d[0], d) for d in [
-            ['Cash - _TC', 1000, 0, '_Test Customer'],
+            ['Cash - _TC', 500, 0, '_Test Customer'],
             ['Interests on Loans - _TC', 0, 500, '_Test Customer'],
-            ['Loans on Collateral - _TC', 0, 500, 'Cash - _TC'],
         ])
         gl_entries = get_loan_payment_gle(payment.name)
         self.assertNotEqual(len(gl_entries), 0)
@@ -50,7 +78,30 @@ class TestLoanPayment(unittest.TestCase):
             self.assertEquals(exp_gle[gle.account][1], gle.debit)
             self.assertEquals(exp_gle[gle.account][2], gle.credit)
             self.assertEquals(exp_gle[gle.account][3], gle.against)
-            self.assertEquals(exp_gle[gle.account][4], self.loan_fixture.name)
+            self.assertEquals(self.loan_name_fixture, gle.against_voucher)
+
+    def test_extends_foreclosure_date(self):
+        first = make_loan_payment(loan=self.loan_name_fixture)
+        payment = make_loan_payment(loan=self.loan_name_fixture)
+        self.fixtures = [first, payment]
+        foreclosure_date = frappe.get_value(
+            'Gold Loan', self.loan_name_fixture, 'foreclosure_date'
+        )
+        self.assertEqual(
+            getdate(foreclosure_date), getdate('2018-08-19')
+        )
+
+    def test_resets_foreclosure_date_on_cancel(self):
+        first = make_loan_payment(loan=self.loan_name_fixture)
+        payment = make_loan_payment(loan=self.loan_name_fixture)
+        payment.cancel()
+        self.fixtures = [first, payment]
+        foreclosure_date = frappe.get_value(
+            'Gold Loan', self.loan_name_fixture, 'foreclosure_date'
+        )
+        self.assertEqual(
+            getdate(foreclosure_date), getdate('2018-07-19')
+        )
 
 
 def make_loan_payment(**kwargs):
@@ -60,17 +111,8 @@ def make_loan_payment(**kwargs):
         'loan': args.loan or '_Test Loan',
         'posting_date': args.posting_date or '2017-09-20',
         'company': args.company or '_Test Company',
-        'paid_amount': 1000.0,
+        'interest_months': args.interest_months or 1,
     })
-    if args.interests:
-        for item in args.interests:
-            doc.append('interests', item)
-    else:
-        doc.append('interests', {
-            'period_label': 'Aug, 2017',
-            'period_code': '2017-08-19 - 2017-09-18',
-            'interest_amount': 500.0,
-        })
     if not args.do_not_insert:
         doc.insert()
         if not args.do_not_submit:
