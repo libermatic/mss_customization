861ccb63c09ec18f174808c60e3d378b84db49c6
Sun Howwrongbum <sun@adrenosapient.com>
2018-03-17 04:03:40 +0530

    Gold Loan: make fields required
    - fill accounts on load

--- mss_customization/mss_loan/doctype/gold_loan/gold_loan.js
+++ mss_customization/mss_loan/doctype/gold_loan/gold_loan.js
@@ -2,7 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Gold Loan', {
-	refresh: function(frm) {
-
-	}
+  refresh: function(frm) {},
+  onload: async function(frm) {
+    const { message: settings = {} } = await frappe.db.get_value(
+      'MSS Loan Settings',
+      null,
+      ['mode_of_payment', 'loan_account', 'interest_income_account']
+    );
+    frm.set_value('mode_of_payment', settings['mode_of_payment']);
+    frm.set_value('loan_account', settings['loan_account']);
+    frm.set_value(
+      'interest_income_account',
+      settings['interest_income_account']
+    );
+  },
 });
