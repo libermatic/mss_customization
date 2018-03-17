9972af2419a1c740bfcaa3a964bee35d10c64872
Sun Howwrongbum <sun@adrenosapient.com>
2018-03-17 06:25:29 +0530

    Loan Summary: create proper

--- mss_customization/mss_loan/report/loan_summary/loan_summary.js
+++ mss_customization/mss_loan/report/loan_summary/loan_summary.js
@@ -1,9 +1,20 @@
-// Copyright (c) 2016, Libermatic and contributors
+// Copyright (c) 2018, Libermatic and contributors
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["Loan Summary"] = {
-	"filters": [
-
-	]
-}
+frappe.query_reports['Loan Summary'] = {
+  filters: [
+    {
+      fieldname: 'customer',
+      label: __('Customer'),
+      fieldtype: 'Link',
+      options: 'Customer',
+    },
+    {
+      fieldname: 'status',
+      label: __('Status'),
+      fieldtype: 'Select',
+      options: '\nOpen\nRepaid\nForeclosed',
+    },
+  ],
+};
--- mss_customization/mss_loan/report/loan_summary/loan_summary.py
+++ mss_customization/mss_loan/report/loan_summary/loan_summary.py
@@ -4,18 +4,55 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import _
+from frappe.utils import today
+from mss_customization.utils.queries import get_paid_interests, get_outstanding
+from mss_customization.utils.transform import month_diff
+
+
+def make_row(current_date):
+    def fn(loan):
+        paid_interests = get_paid_interests(loan.name)
+        latest = paid_interests.get('latest')
+        last_paid_date = latest.get('period_code').split(' - ')[0] \
+            if latest else loan.posting_date
+        unpaid_months = month_diff(current_date, last_paid_date)
+        outstanding = get_outstanding(loan.name)
+        interest = outstanding * loan.interest / 100.0
+        print(latest)
+        return [
+            loan.name,
+            loan.customer,
+            loan.principal,
+            loan.foreclosure_date,
+            outstanding,
+            interest * unpaid_months,
+            unpaid_months,
+            latest and latest.get('posting_date'),
+            latest and latest.get('period_label'),
+        ]
+    return fn
 
 
 def execute(filters=None):
     columns = [
-        _("Loan ID") + ":Link/Gold Loan:160",
+        _("Loan ID") + ":Link/Gold Loan:90",
         _("Customer") + ":Link/Customer:160",
         _("Loan Amount") + ":Currency/currency:90",
+        _("Foreclosure Date") + ":Date:90",
         _("Outstanding") + ":Currency/currency:90",
         _("Current Due") + ":Currency/currency:90",
-        _("Foreclosure Date") + ":Date:90",
+        _("Pending Months") + "::90",
         _("Last Payment Date") + ":Date:90",
-        _("Last Paid Period") + "::120",
+        _("Last Paid Period") + "::160",
     ]
-    data = []
+    loans = frappe.get_list(
+        'Gold Loan',
+        fields=[
+            'name', 'customer', 'principal', 'foreclosure_date', 'interest',
+            'posting_date'
+        ],
+        filters=filters,
+        order_by='posting_date'
+    )
+    data = map(make_row(today()), loans)
     return columns, data
