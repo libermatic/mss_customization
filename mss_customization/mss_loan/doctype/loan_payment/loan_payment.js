b71cd7510e90badf334f735b8c9f2a7e15b073e9
Sun Howwrongbum <sun@libermatic.com>
2018-03-21 01:35:25 +0530

    Loan Payment: handle null loan trying to fetch values

--- mss_customization/mss_loan/doctype/loan_payment/loan_payment.js
+++ mss_customization/mss_loan/doctype/loan_payment/loan_payment.js
@@ -3,14 +3,16 @@
 
 frappe.ui.form.on('Loan Payment', {
   loan: async function(frm) {
-    const { message: loan } = await frappe.db.get_value(
-      'Gold Loan',
-      frm.doc['loan'],
-      ['mode_of_payment', 'loan_account', 'interest_income_account']
-    );
-    frm.set_value('mode_of_payment', loan['mode_of_payment']);
-    frm.set_value('loan_account', loan['loan_account']);
-    frm.set_value('interest_income_account', loan['interest_income_account']);
+    if (frm.doc['loan']) {
+      const { message: loan } = await frappe.db.get_value(
+        'Gold Loan',
+        frm.doc['loan'],
+        ['mode_of_payment', 'loan_account', 'interest_income_account']
+      );
+      frm.set_value('mode_of_payment', loan['mode_of_payment']);
+      frm.set_value('loan_account', loan['loan_account']);
+      frm.set_value('interest_income_account', loan['interest_income_account']);
+    }
   },
   mode_of_payment: async function(frm) {
     if (frm.doc['mode_of_payment']) {
